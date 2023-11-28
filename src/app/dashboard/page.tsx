b533a0281f1e46c8064a79179f0f642bf301ac13
David Hidalgo <juandak16@gmail.com>
2023-11-27 21:36:42 -0400

    feat: init hacker news

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div className="bg-white rounded-lg shadow-lg p-5">
+      <h1>Page</h1>
+      <img src="/images/nextjs.png" alt="Next.js" />
+    </div>
+  );
+}
